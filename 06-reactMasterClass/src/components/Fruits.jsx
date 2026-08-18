09660bac03cf196b21294ca7caa7fc2706e0d43c
Rafay Khan <rafaykhan99999@gmail.com>
2026-08-19 00:57:31 +0500

    feat: update Fruits component to use an array of objects for fruit details and improve rendering logic

--- 06-reactMasterClass/src/components/Fruits.jsx
+++ 06-reactMasterClass/src/components/Fruits.jsx
@@ -1,21 +1,31 @@
 function Fruits() {
-    const fruits = ['Apple', 'Banana', 'Cherry', 'Date', 'Elderberry'];
+    // const fruits = ['Apple', 'Banana', 'Cherry', 'Date', 'Elderberry'];
+    const fruits  = [
+        {name: 'Apple', price: 1.2, emoji: '🍎'},
+        {name: 'Banana', price: 0.5, emoji: '🍌'},
+        {name: 'Cherry', price: 2.0, emoji: '🍒'},
+        {name: 'Date', price: 3.0, emoji: '🌴'},
+        {name: 'Elderberry', price: 1.5, emoji: '🍇'}
+    ]  
+
+    //  this is an array if objects 
     return (
 
     <div>   
         {/* this will do like loos over the fruits array and for each fruit in the 
          array it will return a h1 element with the fruit name in it. */}
+        {/* callback function every returns every single fruit object we have */}
         <ul>
             {fruits.map(fruit => (
-                <li key = {fruit}>
-                    {fruit}
+                <li key = {fruit.name}>
+                    {fruit.name} - ${fruit.price} {fruit.emoji}
                 </li>
             ))}
-        </ul>
+        </ul> 
     </div>
 )
 
 }
-
+ 
 
 export default Fruits
