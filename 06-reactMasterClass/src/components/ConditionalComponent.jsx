61c74821017ed0365588082b636b9a6ca9cc6351
Rafay Khan <rafaykhan99999@gmail.com>
2026-08-19 14:46:50 +0500

    feat: refactor ConditionalComponent to improve readability and maintainability by consolidating return statements

--- 06-reactMasterClass/src/components/ConditionalComponent.jsx
+++ 06-reactMasterClass/src/components/ConditionalComponent.jsx
@@ -2,13 +2,16 @@ import Introduction from './Introduction';
 import Welcome from './Welcome';
 
 export default function ConditionalComponent() {
+    let msg;
     const display = true
 
+    //  by doing this it will eleiminate the need of two retuern statements and will make the code more readable and maintainable.
     if(display) {
-        return <Welcome />
+        msg =<Welcome />
     } 
     else if (!display)
     {
-        return <Introduction />
+        msg =  <Introduction />
     }
+    return msg;
 }
