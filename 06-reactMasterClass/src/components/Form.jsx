3c245ec10afeaed7147f34a6eca9cc554380251b
Rafay Khan <rafaykhan99999@gmail.com>
2026-08-20 01:01:24 +0500

    feat: enhance Form component structure and improve input handling

--- 06-reactMasterClass/src/components/Form.jsx
+++ 06-reactMasterClass/src/components/Form.jsx
@@ -1,24 +1,47 @@
-import { useState } from "react"
+import { useState } from "react";
 
 export default function Form() {
+  const [inputFormName, setInputFormName] = useState({
+    firstName: "",
+    lastName: "",
+  });
 
-    const [inputFormName, setInputFormName] = useState({firstName: "", lastName: ""});
+  // function handleChange(e) {
+  //     setInputFormName(firstName.target.value)
 
-    // function handleChange(e) {
-    //     setInputFormName(firstName.target.value)
+  // }
+  function handleSubmition(e) {
+    e.preventDefault(); // so this will prevent the page from reloading when we submit the form and it will
+    //  allow us to handle the form submission in our own way.
+    console.log(inputFormName);
+  }
 
-    // }
+  return (
+    <>
+      <h1>THIS IS THE FORM!</h1>
+      <div>
+        <form>
+          {" "}
+          <input
+            onChange={(e) =>
+              setInputFormName({ ...inputFormName, firstName: e.target.value })
+            }
+            type="text"
+            value={inputFormName.firstName}
+          />
+          first name
+          <input
+            onChange={(e) =>
+              setInputFormName({ ...inputFormName, lastName: e.target.value })
+            }
+            type="text"
+            value={inputFormName.lastName}
+          />
+          last name
 
-    return (
-        <>
-            <h1>
-                THIS IS THE FORM!
-            </h1>
-            <div>
-                <form> <input onChange={(e) => setInputFormName({firstName: e.target.value})} type="text" value = {inputFormName.firstName} />first name</form>
-                <br />
-                <form> <input onChange={(e) => setInputFormName({lastName: e.target.value})} type="text" value = {inputFormName.lastName} />last name</form>
-            </div>
-        </>
-    )
+          <button onClick={(e)=>handleSubmition(e)}>submit</button>
+        </form>
+      </div>
+    </>
+  );
 }
