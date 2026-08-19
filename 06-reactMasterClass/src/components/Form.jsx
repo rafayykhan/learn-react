7d62663006cd034064510cf3228f3dded68b184e
Rafay Khan <rafaykhan99999@gmail.com>
2026-08-19 23:49:58 +0500

    feat: update Form component to manage first and last name inputs separately

--- 06-reactMasterClass/src/components/Form.jsx
+++ 06-reactMasterClass/src/components/Form.jsx
@@ -2,20 +2,21 @@ import { useState } from "react"
 
 export default function Form() {
 
-    const [inputFormName, setInputFormName] = useState("");
+    const [inputFormName, setInputFormName] = useState({firstName: "", lastName: ""});
 
     function handleChange(e) {
         setInputFormName(e.target.value)
-
     }
-    
+
     return (
         <>
             <h1>
                 THIS IS THE FORM!
             </h1>
             <div>
-                <form> <input onChange={(e) => handleChange(e)} type="text" value = {inputFormName} /></form>
+                <form> <input onChange={(e) => handleChange(e)} type="text" value = {inputFormName.firstName} />first name</form>
+                <br />
+                <form> <input onChange={(e) => handleChange(e)} type="text" value = {inputFormName.lastName} />last name</form>
             </div>
         </>
     )
