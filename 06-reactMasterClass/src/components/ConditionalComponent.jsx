98e1d08da65a07d946d8ee60bef86be727d52cfc
Rafay Khan <rafaykhan99999@gmail.com>
2026-08-19 15:14:04 +0500

    feat: refactor ConditionalComponent to use a ternary operator for conditional rendering and improve code readability

--- 06-reactMasterClass/src/components/ConditionalComponent.jsx
+++ 06-reactMasterClass/src/components/ConditionalComponent.jsx
@@ -2,16 +2,17 @@ import Introduction from './Introduction';
 import Welcome from './Welcome';
 
 export default function ConditionalComponent() {
-    let msg;
+    // let msg;
     const display = true
-
+    let msg = display ? <Welcome /> : <Introduction />; // here we are using a ternary operator to conditionally render 
+    // either the Welcome component or the Introduction component based on the value of the display variable.
     //  by doing this it will eleiminate the need of two retuern statements and will make the code more readable and maintainable.
-    if(display) {
-        msg =<Welcome />
-    } 
-    else if (!display)
-    {
-        msg =  <Introduction />
-    }
+    // if(display) {
+    //     msg =<Welcome />
+    // } 
+    // else if (!display)
+    // {
+    //     msg =  <Introduction />
+    // }
     return msg;
 }
