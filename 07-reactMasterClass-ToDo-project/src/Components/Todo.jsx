7aebcc5d12bdb05dd4c56a19662976e2af4f52b4
Rafay Khan <rafaykhan99999@gmail.com>
2026-08-21 02:10:52 +0500

    feat: refactor Todo state to use object structure for improved handling and reset logic

--- 07-reactMasterClass-ToDo-project/src/Components/Todo.jsx
+++ 07-reactMasterClass-ToDo-project/src/Components/Todo.jsx
@@ -3,7 +3,7 @@ import TodoItems from "./TodoItems";
 
 export default function Todo() {
 //   const [toDo, setToDo] = useState("");
-  const [toDo, setToDo] = useState("");
+  const [toDo, setToDo] = useState({name:"", done: false});
 
 
   // we want to store the list somewhere so we can display it on the screen and we can also add new items
@@ -16,14 +16,15 @@ export default function Todo() {
   //  of toDo is set to an empty string. and we are also creating a state variable called toDoList and a
   // function called setToDoList to update the value of toDoList. The initial value of toDoList is set to an empty array.
 
-  function handleClick(e) {
+  function handleClick(e, item) {
     e.preventDefault(); // this will prevent the page from reloading when we submit the form and it will allow us to handle the form submission in our own way.
     // why we did that because the todo was a string and we cant use it directly instead
     // i have made a new array and added the string to it and then updated the state of the array with the new array.
     setToDoList([...toDoList, toDo]); // here we are using the spread operator to create a new array with the existing items in the toDoList array and the new
     // item that we want to add to the list. and we are also updating the value of toDoList with the new array that we created.
     // console.log("clicked")
-    setToDo("");
+    setToDo({ name: "", done: false }); // reset with the same shape as the state, not a plain string
+    console.log("added the: ", item);
   }
 
 
@@ -45,8 +46,8 @@ export default function Todo() {
     >
       <form style={{ display: "flex", gap: "8px" }}>
         <input
-          onChange={(e) => setToDo(e.target.value)}
-          value={toDo}
+          onChange={(e) => setToDo({name: e.target.value , done: false })}
+          value={toDo.name}
           type="text"
           style={{
             flex: 1,
@@ -57,7 +58,7 @@ export default function Todo() {
           }}
         />
         <button
-          onClick={(e) => handleClick(e)}
+          onClick={(e) => handleClick(e, toDo)} // pass the current todo so handleClick actually receives an item
           type="submit"
           style={{
             padding: "10px 18px",
@@ -87,7 +88,7 @@ export default function Todo() {
               }}
             >
               <div style={{ flex: 1 }}>
-                <TodoItems item={item} index={index} />
+                <TodoItems item={item.name} index={index} /> {/* pass the string, TodoItems cant render an object */}
               </div>
               <button
                 style={{
