9eb433808587cc0411c43240b8f51cb3ace77eb7
Rafay Khan <rafaykhan99999@gmail.com>
2026-08-22 02:14:34 +0500

    feat: implement FoodList and FoodItem components, and enhance Search with API integration

--- 08-reactMasterClass-FoodApp-Project/src/App.jsx
+++ 08-reactMasterClass-FoodApp-Project/src/App.jsx
@@ -1,15 +1,18 @@
-import Search from "./Components/Search"
+import { useState } from "react";
+import Search from "./Components/Search";
+import FoodList from "./Components/FoodList";
 
 function App() {
-
+  const [dataList, setDataList] = useState([]); // to save the api data
 
   return (
     <>
-     <div>
-        <Search/>
-     </div>
+      <div>
+        <Search setDataList={setDataList} />
+        <FoodList dataList = {dataList}/>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
