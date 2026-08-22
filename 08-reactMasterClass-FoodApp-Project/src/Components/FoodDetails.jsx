973056f40270fc82bca0b13d20a2ef901bcfa84e
Rafay Khan <rafaykhan99999@gmail.com>
2026-08-22 09:07:57 +0500

    style: refactor FoodDetails component for improved readability and structure

--- 08-reactMasterClass-FoodApp-Project/src/Components/FoodDetails.jsx
+++ 08-reactMasterClass-FoodApp-Project/src/Components/FoodDetails.jsx
@@ -1,26 +1,49 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 
-export default function FoodDetails({foodId}) {
-    const [foodData, setFoodData] = useState({})
-    const URL = `https://api.spoonacular.com/recipes/${foodId}/information`
-    const API_KEY = "645338d003c7421bb29ea0a053dfafda"
+export default function FoodDetails({ foodId }) {
+  const [foodData, setFoodData] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+  const URL = `https://api.spoonacular.com/recipes/${foodId}/information`;
+  const API_KEY = "645338d003c7421bb29ea0a053dfafda";
 
-    useEffect(()=>{
-        async function fetchFood(){
-          const response = await fetch(`${URL}?apiKey=${API_KEY}` )
-          const responseData = await response.json();
-        //   setDataList(responseData.results ?? [])
-            setFoodData(responseData)
-        }
-        fetchFood()
-      }, [foodId])
+  useEffect(() => {
+    async function fetchFood() {
+      const response = await fetch(`${URL}?apiKey=${API_KEY}`);
+      const responseData = await response.json();
+      //   setDataList(responseData.results ?? [])
+      setFoodData(responseData);
+      setIsLoading(false);
+    }
+    fetchFood();
+  }, [foodId]);
 
-    return(
-        <div>
-            Food Details
-            {foodId} 
-            {foodData.title}
-            <img src={foodData.image} alt="" />
-        </div>
-    )
+  return (
+    <div>
+      <div>
+        <h1>{foodData.title}</h1>
+        <img src={foodData.image} alt="" />
+        <span>
+          <strong>{foodData.readyInMinutes} Minutes</strong>
+        </span>
+        <span>{foodData.vegetarian ? "Vegetarian" : "Non-Vegetarian"}</span>
+        <span>
+          {" "}
+          <strong>Serves {foodData.servings}</strong>
+        </span>
+      </div>
+      <div>
+        <h2>
+          INSTRUCTIONS
+          {isLoading ? (
+            <p>Loading...</p>
+          ) : (
+            foodData.analyzedInstructions?.[0]?.steps?.map((step) => (
+              <li key={step.number}>{step.step}</li>
+            ))
+          )}
+        </h2>
+      </div>
+      {/* {foodId} */}
+    </div>
+  );
 }
