fba8a734714584e55bc27465e9f61f56bf9dc1c6
Rafay Khan <rafaykhan99999@gmail.com>
2026-08-22 16:38:55 +0500

    feat: implement useReducer for state management and remove unused assets

--- 09-reactMasterClass/src/App.jsx
+++ 09-reactMasterClass/src/App.jsx
@@ -1,122 +1,58 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { useReducer } from "react";
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
+  const [state, dispatch] = useReducer(reducer, 0)  
+  function reducer(state, action)
+  {
+    // the jib of the reducer function is to cahnge the value of the reducer funnction 
+    if (action.type == "increment"){
+      // 
+      return state + action.payload;
+    }
+    
+    if (action.type == "decrement"){
+      // 
+      return state - action.payload;
+    }
+    
+  } 
   return (
     <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
+      <div>
         <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
+          <h1>
+            UseState: {count}
+           
+          </h1>
         </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
+
+        <button onClick={() => setCount(count + 1)}>
+          <h1> Increase</h1>
         </button>
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
+        <button onClick={() => setCount(count - 1)}>
+          <h1> Decrement</h1>
+        </button>
+        <div>
+          <h1>
+             Reducer: {state}
+          </h1>
         </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
+        {/* dispatch whenever i click on the increment this action is going to be dipatched and the action is being 
+        handleld by the reducer function*/}
+        {/*  when ever we are clicking we are dispatching the action object
+         and reducer function handle this action*/}
+        <button onClick={()=>dispatch({type: "increment", payload: 1})}>
+          <h1> Increase</h1>
+        </button>
+        <button onClick={()=>dispatch({type: "decrement", payload: 1})}>
+          <h1> Decrement</h1>
+        </button>
+      </div>
     </>
-  )
+  ); 
 }
 
-export default App
+export default App;
